6bd273953f524e4890fbfa0fbd25f8441bd6b27b
Engineer <jasonmrimer@gmail.com>
2021-02-20 17:07:27 -0600

    used HttpUnit with ServletRunner and added Rhino to get rid of test runner error. generator test passes, use for future tests!

--- src/main/webapp/assignment2/sidebar.jsp
+++ src/main/webapp/assignment2/sidebar.jsp
@@ -37,9 +37,4 @@
     #sidebar-container {
         min-height: 100vh;
     }
-    .navigation {
-        display: flex;
-        flex-direction: column;
-        align-items: flex-start;
-    }
 </style>
--- src/test/java/assignment2/GreetingGeneratorTest.java
+++ src/test/java/assignment2/GreetingGeneratorTest.java
@@ -1,13 +1,18 @@
 package assignment2;
 
+import com.meterware.httpunit.GetMethodWebRequest;
+import com.meterware.httpunit.WebRequest;
+import com.meterware.servletunit.InvocationContext;
+import com.meterware.servletunit.ServletRunner;
+import com.meterware.servletunit.ServletUnitClient;
 import org.junit.jupiter.api.Test;
+import org.xml.sax.SAXException;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.io.StringWriter;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -23,19 +28,41 @@ class GreetingGeneratorTest {
   public void returnsMessageWithDate() throws ServletException, IOException {
     HttpServletRequest request = mock(HttpServletRequest.class);
     HttpServletResponse response = mock(HttpServletResponse.class);
-    String expectedMessage = "Hi CSE640 2021 Class";
-
-    StringWriter stringWriter = new StringWriter();
-    PrintWriter printWriter = new PrintWriter(stringWriter);
-    when(response.getWriter()).thenReturn(printWriter);
+    HttpSession session = mock(HttpSession.class);
+    when(request.getSession()).thenReturn(session);
 
     servlet = new GreetingGenerator();
     servlet.doGet(request, response);
-    String result = stringWriter.getBuffer().toString().trim();
+    String result = request.getSession().getAttribute("greeting").toString();
+
+    String expectedMessage = "Hi CSE640 2021 Class";
     assertTrue(result.contains(expectedMessage));
 
     String dateString = result.replace(expectedMessage, "").trim();
     assertTrue(isValid(dateString), "No date found in " + dateString);
+  }
+
+  @Test
+  public void testingWithServletRunner() throws IOException, ServletException {
+    ServletRunner servletRunner = new ServletRunner();
+    servletRunner.registerServlet("GreetingGenerator", GreetingGenerator.class.getName());
+    ServletUnitClient servletUnitClient = servletRunner.newClient();
+    WebRequest request = new GetMethodWebRequest("http://fake.test/GreetingGenerator");
+    InvocationContext invocationContext = servletUnitClient.newInvocation(request);
+    GreetingGenerator servlet = (GreetingGenerator) invocationContext.getServlet();
+    HttpServletRequest httpServletRequest = invocationContext.getRequest();
+    HttpServletResponse httpServletResponse = invocationContext.getResponse();
+    HttpSession session = httpServletRequest.getSession();
+
+    servlet.doGet(httpServletRequest, httpServletResponse);
+
+    String expectedGreeting = "Hi CSE640 2021 Class";
+    String actualGreeting = session.getAttribute("greeting").toString();
+    assertTrue(actualGreeting.contains(expectedGreeting));
+
+    String dateString = actualGreeting.replace(expectedGreeting, "").trim();
+    assertTrue(isValid(dateString), "No date found in " + dateString);
+
   }
 
   private boolean isValid(String dateStr) {
